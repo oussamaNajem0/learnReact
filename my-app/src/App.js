f94a1d1e45bae0e5d97561ff60132b6cc893f814
oussamaNajem0 <oussamanajem91@gmail.com>
2022-12-25 22:57:22 +0100

    createRout

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,14 +4,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1 className="title">Hello World</h1>
+      <h2 className="h2" >Oussama</h2>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus alias
         itaque ratione magni voluptas dicta totam vel maxime, harum rem placeat
         officia accusamus voluptatem error impedit fugiat similique sunt.
         Aliquid! 
       </p>
-      <button>like</button>
+      <button>like</button> <button>Dislike</button>
     </div>
   );
 }
