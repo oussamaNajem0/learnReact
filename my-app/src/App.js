0c704cbd3bd57d1a88462aea13719cac5b6e34fc
oussamaNajem0 <oussamanajem91@gmail.com>
2022-12-25 16:31:52 +0100

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus alias
+        itaque ratione magni voluptas dicta totam vel maxime, harum rem placeat
+        officia accusamus voluptatem error impedit fugiat similique sunt.
+        Aliquid! 
+      </p>
+      <button onClick={() => this.setState({ liked: true })}>like</button>
+    </div>
+  );
+}
+
+export default App;
