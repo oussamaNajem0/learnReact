af3e07326328aee554d6d126e3efd3689ea428c4
oussamaNajem0 <oussamanajem91@gmail.com>
2022-12-26 20:23:25 +0100

    componements

---
+++ my-app/src/comp/footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <footer>
+                Designed and developed by Oussama
+                <span>🧡</span>
+            </footer>
+        </div>
+    );
+}
+
+export default Footer;
