6fee4652e188325f10c9f377eaf2144ce79f5b3a
oussamaNajem0 <oussamanajem91@gmail.com>
2022-12-26 11:24:49 +0100

    Project-Courses

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,15 +4,91 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 className="title">Hello World</h1>
-      <h2 className="h2" >Oussama</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus alias
-        itaque ratione magni voluptas dicta totam vel maxime, harum rem placeat
-        officia accusamus voluptatem error impedit fugiat similique sunt.
-        Aliquid! 
-      </p>
-      <button>like</button> <button>Dislike</button>
+      <header className="hide-when-mobile">
+          <h1>Courses</h1>
+          <ul className="flex">
+            <li className="main-list">
+              <a className="main-link" href="#"> HTML </a>
+              <ul className="sub-ul">
+                <li><a href>Full Course</a></li>
+                <li><a href>Crash Course</a></li>
+                <li><a href>learn in 1h</a></li>
+              </ul>
+            </li>
+            <li className="main-list">
+              <a className="main-link" href="#"> CSS </a>
+              <ul className="sub-ul">
+                <li><a href>Full Course</a></li>
+                <li><a href>CSS Examples</a></li>
+                <li className="mini-projects">
+                  <a href>mini projects&nbsp; + </a>
+                  <ul className="sub-sub-ul">
+                    <li><a href>project 1</a></li>
+                    <li><a href>project 2</a></li>
+                    <li><a href>project 3</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </li>
+            <li className="main-list">
+              <a className="main-link" href="#"> JavaScript </a>
+              <ul className="sub-ul sub-of-js">
+                <li><a href>coming soon🔥</a></li>
+              </ul>
+            </li>
+          </ul>
+        </header>
+        <header className="show-when-mobile">
+          <h1>Courses</h1>
+          <label className="absolute" htmlFor="burger">
+            <i className="fas fa-bars" />
+          </label>
+          <input id="burger" type="checkbox" />
+          <div className="show-on-click">
+            <div className="main-div">
+              <label htmlFor="html">HTML <i className="fas fa-plus" /> </label>
+              <input id="html" type="checkbox" />
+              <ul className="sub-div">
+                <li><a href>Full Course</a></li>
+                <li><a href>Crash Course</a></li>
+                <li><a href>learn in 1h</a></li>
+              </ul>
+            </div>
+            <div className="main-div">
+              <label htmlFor="css">CSS <i className="fas fa-plus" /> </label>
+              <input id="css" type="checkbox" />
+              <ul className="sub-div">
+                <li><a href>Full Course</a></li>
+                <li><a href>CSS Examples</a></li>
+                <li>
+                  <label className="mini-projects" htmlFor="mini">mini projects <i className="fas fa-plus" /> </label>
+                  <input id="mini" type="checkbox" />
+                  <ul className="sub-sub-div">
+                    <li><a href>project 1</a></li>
+                    <li><a href>project 2</a></li>
+                    <li><a href>project 3</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+            <div className="main-div">
+              <label htmlFor="js">JavaScript <i className="fas fa-plus" /> </label>
+              <input id="js" type="checkbox" />
+              <ul className="sub-div">
+                <li><a href>coming soon🔥</a></li>
+              </ul>
+            </div>
+          </div>
+        </header>
+        <main>
+          Respossive Drop-down <br />
+          Menu Bar <br />
+          HTML &amp; CSS only
+        </main>
+        <footer>
+          Designed and developed by Oussama
+          <span>🧡</span>
+        </footer>
     </div>
   );
 }
